7cc618ce5ad2c133631df7d3578fbfe9a5a56aef
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-03-25 23:38:39 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🔕 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🏋️‍♀️", url="https://t.me/pagal_faimly"
+                text="🔕 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🏋️‍♀️", url="https://t.me/Noobxcoders"
             ),
         ],
         [
@@ -123,7 +123,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🏵️ 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🌺", url="https://t.me/pagal_faimly"
+                text="🏵️ 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🌺", url="https://t.me/Noobxcoders"
             ),
         ],
         [
@@ -157,7 +157,7 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽➡️", url="https://t.me/pagal_faimly"
+                text="𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽➡️", url="https://t.me/Noobxcoders"
             ),
         ],
         [
@@ -188,7 +188,7 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⏮️ 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽▶️", url="https://t.me/pagal_faimly"
+                text="⏮️ 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽▶️", url="https://t.me/Noobxcoders"
             ),
         ],
         [
@@ -348,7 +348,7 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/pagal_faimly"
+                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/Noobxcoders"
             ),
         ],
         [
