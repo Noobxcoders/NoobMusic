7c3847bd14eb66083367b3cc942b048e220e1acb
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-06-02 00:59:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/90e6fa5d69b05162e3666.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/250806f452565f9a93de7.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://graph.org/file/90e6fa5d69b05162e3666.jpg"
+            START_IMG_URL = "https://graph.org/file/250806f452565f9a93de7.jpg"
