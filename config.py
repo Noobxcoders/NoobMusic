c143a31699fd79775ad6245262637c378eefe0f0
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-06-15 12:50:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Noobxcoders/NoobMusi
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Noobxcoders")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Noobxcoders")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GifsGallary")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Amazingdpzworld")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
